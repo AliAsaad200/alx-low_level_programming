be1c46e4ee6f00dde5da64de1bcda8cbcade2891
AliAsaad200 <you@example.com>
2023-09-07 22:50:47 +0300

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -7,6 +7,6 @@
 int main(void)
 {
 	char *mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	 write(2, mess, 59);
-	return (0);
+	 write(2, mess, 63);
+	return (1);
 }
