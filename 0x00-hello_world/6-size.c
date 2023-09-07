076e2e933c12f5bb04ec5ea8252349aac766fc96
AliAsaad200 <you@example.com>
2023-09-07 19:35:28 +0300

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,6 @@ int main(void)
 	printf("Size of long: %ld byte(s)\n", sizeof(long));
 	printf("Size of long long: %d byte(s)\n", sizeof(long long));
 	printf("Size of float: %d byte(s)\n", sizeof(float));
-	printf("Size of double: %d byte(s)\n", sizeof(double));
-	printf("Size of long double: %d byte(s)\n", sizeof(long double));
 	return (0);
 }
 
