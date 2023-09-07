afa4c23c22059533e8421718bea19dc00070efa5
AliAsaad200 <you@example.com>
2023-09-07 22:44:17 +0300

    updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	 write(2, mess, 59);
+	return (0);
+}
