8e9daf6cce2054b0636fe009929aaf43003bd9d6
AliAsaad200 <you@example.com>
2023-09-08 18:42:57 +0300

    wewe

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp = "a";
+
+	while (alp <= "z")
+	{
+		putchar("%c ");
+	}
+	return (0);
+	}
