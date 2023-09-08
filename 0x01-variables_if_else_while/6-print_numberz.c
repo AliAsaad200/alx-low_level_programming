fbd000d6dab95066a11a0c2fe4d44358a8d92953
AliAsaad200 <you@example.com>
2023-09-08 19:49:30 +0300

    wewe

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alp;
+
+	for (alp = 0; alp < 10 ; alp++)
+		putchar(alp + '0');
+
+	putchar('\n');
+	return (0);
+}
