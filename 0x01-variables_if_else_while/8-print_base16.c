0a2917a7c6240678e874587f45290f98f597b978
AliAsaad200 <you@example.com>
2023-09-08 20:47:29 +0300

    wewe

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[16] = "0123456789abcdef"
+	char c;
+
+	for (c = 0; i < 16; i++)
+	{
+		putchar(alp[i]);
+
+	} putchar("\n");
+	return (0);
+}
