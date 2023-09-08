a72ab3932664e3594d91e265c10ed58ee470c095
AliAsaad200 <you@example.com>
2023-09-08 19:01:31 +0300

    wewe

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char alp;
 
-	for (alp="a";alp <= "z"; alp++)
-	{
-		putchar("%c" );
-	}
+	for (alp = 'a';alp <= 'z'; alp++)
+		putchar(alp);
+
+	putchar('\n');
 	return (0);
-	}
+}
