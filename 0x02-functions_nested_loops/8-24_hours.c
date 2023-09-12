79f17df08a0451fb99d7f5d19008a02b582a488d
AliAsaad200 <you@example.com>
2023-09-12 21:35:53 +0300

    w

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,14 @@ void jack_bauer(void)
 {
 	int min;
 	int hr;
+
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar(min + '0');
-			for (hr = 0; hr <= 23 ; hr++)
-		{
-			_putchar(hr + '0');
-			_putchar(',');
-		}
+			_putchar(min + '0')
+			for (hr = 0; hr < 24; hr++)
+			{
+				_putchar(hr + '0');
+				_putchar(':');
+			}
 			_putchar('\n');
-	}
-
-	
-}
+		}
