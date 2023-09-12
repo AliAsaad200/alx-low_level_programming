c893a5b78f49dbe01debcde91a113b48a5c20cfb
AliAsaad200 <you@example.com>
2023-09-12 22:13:46 +0300

    w

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *times_table - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int i;
+	int num;
+	int result;
+
+		for (num = 0; hr <= 9; num++)
+		{
+			for (i = 0; i <= 9; i++)
+			{
+				result = i * num;
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+				_putchar(':');
+				_putchar('\n');
+			}
+		}
+}
