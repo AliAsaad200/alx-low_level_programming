c33060d833c5f7be0d4b6150bea2f034b2b62796
AliAsaad200 <you@example.com>
2023-09-12 22:42:39 +0300

    w

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - Entry point
+ *@c: The loweer to checked
+ * Return: Always 0 (Success)
+ */
+int add(int, int)
+{
+	int a;
+	int b;
+	int result;
+	result = a + b ;
+	return (0);
+}
