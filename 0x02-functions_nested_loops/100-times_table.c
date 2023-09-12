50d1a0ead1ab234ebac5767a2fc46c8d2ec93a98
AliAsaad200 <you@example.com>
2023-09-12 23:55:35 +0300

    w

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,20 +7,42 @@
  */
 void print_times_table(int n)
 {
-	int row, col;	 
+	int num1, num2, Result;
 
-  if (n > 15 || n < 0) {
-    return;
-  }
-	
-  for ( row = 0; row <= n; row++) {
-    for (col = 0; col <= n; col++) {
-      int result = row * col;
-      printf("%d", result);
-      if (col < n) {
-        printf(",  ");
-      }
-    }
-    printf("\n");
-  }
+	if (n > 15 || n < 0)
+	return;
+	while (num1 <= n)
+	{
+	for (num2 = 0; num2 <= n; num2++)
+	{
+		Result = num1 * num2;
+		if (Result > 99)
+	{
+		_putchar(Result / 100 + '0');
+		_putchar((Result / 10 % 10) + '0');
+		_putchar(Result % 10 + '0');
+	}
+	else if (Result > 9)
+	{
+		_putchar(' ');
+		_putchar(Result / 10 + '0');
+		_putchar(Result % 10 + '0');
+	}
+	else if (num2 != 0)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(Result + '0');
+	}
+	else
+	    _putchar(Result + '0');
+	if (num2 != n)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	}
+	_putchar('\n');
+	num1++;
+	}
 }
