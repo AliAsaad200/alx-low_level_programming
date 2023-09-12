1737321f3194085807383ad3ac4a9a9db67f6fc0
AliAsaad200 <you@example.com>
2023-09-12 20:45:30 +0300

    w

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,17 +6,12 @@
  */
 int _abs(int n)
 {
-	if (n < 0)
+	if (n >= 0)
 	{
-		_putchar(-n);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
+		return (n);
 	}
 	else
 	{
-		_putchar(n);
+		return (-n);
 	}
-	return (0);
 }
