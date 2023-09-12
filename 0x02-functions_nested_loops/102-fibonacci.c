103b9c1bdb632f2c97a4d9047b8473495100c26b
AliAsaad200 <you@example.com>
2023-09-13 00:10:30 +0300

    w

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - 50 fibonacci numbers
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	long num1, num2, Sum, Contuer;
+
+	num1 = 0;
+	num2 = 1;
+	for (Contuer = 0; Contuer < 50; Contuer++)
+	{
+	Sum = num1 + num2;
+	num1 = num2;
+	num2 = Sum;
+	if (Contuer != 49)
+		printf("%ld, ", Sum);
+	else
+		printf("%ld\n", Sum);
+	}
+	return (0);
+}
