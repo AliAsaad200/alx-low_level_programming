4e430e8829eabee252de5262e286bd13670bbfff
AliAsaad200 <you@example.com>
2023-09-14 13:18:48 +0300

    w

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - check the code.
+ *@c: The loweer to checked
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	c = 0;
+	if (c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
