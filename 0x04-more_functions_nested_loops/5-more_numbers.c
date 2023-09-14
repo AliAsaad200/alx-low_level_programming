0f6bdc727342e706aa5a240f5621ca2c9a97159d
AliAsaad200 <you@example.com>
2023-09-14 14:18:00 +0300

    w

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,12 @@ void more_numbers(void)
 	{
 		for (i = '0'; i <= '14'; i++)
 	{
-		if (i < 9)
+		if (i > '9')
 		{
 			_putchar( i / 10);
 			_putchar( i % 10);
-		}else
+		}
+		else
 		{
 			_putchar(i);
 		}
