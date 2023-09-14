9b72ac3761bd8db78ec125bf5505998b40e0d6fa
AliAsaad200 <you@example.com>
2023-09-14 13:38:31 +0300

    w

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - check the code.
+ *@a: The loweer to checked
+*@b: The loweer to checked
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
