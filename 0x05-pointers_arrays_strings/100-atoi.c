103eb9863c1fef12a62bea120ffede82ecda7a60
AliAsaad200 <you@example.com>
2023-09-18 22:07:39 +0300

    k

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _atoi - integer is positive or negative
+* @s: first integer
+* Return: 0
+*/
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
