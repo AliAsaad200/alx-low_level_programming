d0290f3bdb1b81a9ff0841d48a5e53da2d692f92
AliAsaad200 <you@example.com>
2023-09-18 17:28:29 +0300

    k

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * reset_to_98 - integer is positive or negative
 * @n: first integer
@@ -8,6 +9,6 @@
 void reset_to_98(int *n)
 {
 	n = *&n;
-	_putchar(n);
+	printf("%d", n);
 
 }
