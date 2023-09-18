9725e17d9afdcb05ac9bc3486fd5fce140ca30ff
AliAsaad200 <you@example.com>
2023-09-18 19:15:51 +0300

    k

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
 * print_rev - integer is positive or negative
 * @s: first integer
@@ -8,8 +9,8 @@
 void print_rev(char *s)
 {
 	int i;
-
-	for (i = *s.length - 1; i >= 0; i--)
+	int len = strlen (s);
+	for (i = len - 1; i >= 0; i--)
 	{
 		putchar (s[i]);
 	}
