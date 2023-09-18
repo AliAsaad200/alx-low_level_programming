994b575817329e7e8bab1a429942a9d8b150c636
AliAsaad200 <you@example.com>
2023-09-18 18:39:37 +0300

    k

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - integer is positive or negative
+* @str: first integer
+* Return: 0
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for(i = 0; i < str; i++)
+	{
+		str[i];
+	}
+}
