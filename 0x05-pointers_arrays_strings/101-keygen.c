de3a16c5713382b27f889ab3ff6245b8380224ab
AliAsaad200 <you@example.com>
2023-09-18 23:04:07 +0300

    k

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,23 @@
  */
 int main(void)
 {
-	int n;
+	char password[100];
+	int i, length;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	srand(time(NULL));
+	length = rand() % 9 + 6;
+	for (i = 0; i < length; i++)
 	{
-		printf("Wrong password");
-	} else
-	{
-		printf("Tada! Congrats");
+		int type = rand() % 3;
+
+	if (type == 0)
+		password[i] = 'a' + (rand() % 26);
+	else if (type == 1)
+		password[i] = 'A' + (rand() % 26);
+	else
+		password[i] = '0' + (rand() % 10);
 	}
+	password[i] = '\0';
+	printf("%s\n", password);
 	return (0);
 }
