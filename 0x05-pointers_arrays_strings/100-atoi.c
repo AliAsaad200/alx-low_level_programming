32348b6e50a9d9e883210ba249539f0e5c0901a5
AliAsaad200 <you@example.com>
2023-09-18 22:29:49 +0300

    k

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,5 +8,31 @@
 */
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	if (result == 0 && (s[i] < '0' || s[i] > '9'))
+	{
+		return (0);
+	}
+	return (sign * result);
 }
