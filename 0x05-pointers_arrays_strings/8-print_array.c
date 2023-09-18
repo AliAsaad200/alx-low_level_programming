287e11803d6ba05aaa7897a23a3e3693b304f72b
AliAsaad200 <you@example.com>
2023-09-18 20:28:55 +0300

    k

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* print_array - integer is positive or negative
+* @a: first integer
+* @n: first integer
+* Return: 0
+*/
+void print_array(int *a, int n)
+{
+	int len = strlen(a);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+}
