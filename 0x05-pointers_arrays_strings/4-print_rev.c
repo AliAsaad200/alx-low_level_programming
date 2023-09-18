c7b8dc1954826fb3fc8a7d5587444ff0c2855414
AliAsaad200 <you@example.com>
2023-09-18 18:48:01 +0300

    k

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_rev - integer is positive or negative
+* @s: first integer
+* Return: 0
+*/
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i--)
+	{
+		putchar (str[i]);
+	}
+	putchar ('\n');
+}
