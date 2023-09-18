59c93b96a4a5d510e8c5bdcc37b67b52ea82a914
AliAsaad200 <you@example.com>
2023-09-18 20:50:16 +0300

    k

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strcpy - integer is positive or negative
+* @src: first integer
+* @dest: first integer
+* Return: 0
+*/
+char *_strcpy(char *dest, char *src)
+{
+	char *a = dest;
+	*dest = '\0';
+	return (a);
+}
