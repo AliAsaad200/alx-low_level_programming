d1877f297a183768e2e95274ad3d080b4d98dc06
AliAsaad200 <you@example.com>
2023-09-20 21:17:46 +0300

    w

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strcmp - integer is positive or negative
+* @s1: first integer
+* @s2: first integer
+* Return: 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
