b4588bc9fccdb22ae13e734fec753c7d8e87e42b
AliAsaad200 <you@example.com>
2023-09-22 00:36:00 +0300

    w

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -16,19 +16,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int sum = 0;
 
 	while (n1[n1_len] != '\0')
-	{
-		n1_len++;
-	}
+	n1_len++;
 	while (n2[n2_len] != '\0')
-	{
-		n2_len++;
-	}
-	index = size_r - 1;
+	n2_len++;
 
+	index = size_r - 1;
 	if (n1_len > size_r - 1 || n2_len > size_r - 1)
-	{
-		return (0);
-	}
+	return (0);
 	r[index] = '\0';
 	while (index > 0)
 	{
