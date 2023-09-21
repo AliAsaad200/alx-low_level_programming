cf6ea1c2c2ab09e9b1758b71f7fcd41fb91cfd08
AliAsaad200 <you@example.com>
2023-09-22 00:27:18 +0300

    w

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-/**
-* main - integer is positive or negative
-*@a - www
-* Return: 0
-*/
+
 int main(void)
 {
 	int n;
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+* infinite_add - integer is positive or negative
+* @n1: first integer
+* @n2: first integer
+* @r: first integer
+* @size_r: first integer
+* Return: 0
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int carry = 0;
+	int i = 0;
+	int n1_len = 0;
+	int n2_len = 0;
+
+	while (n1[n1_len] != '\0')
+	n1_len++;
+	while (n2[n2_len] != '\0')
+	n2_len++;
+	int index = size_r - 1;
+
+	if (n1_len > size_r - 1 || n2_len > size_r - 1)
+	return (0);
+	r[index] = '\0';
+	while (index > 0)
+	{
+		int num1 = 0;
+		int num2 = 0;
+	if (n1_len > 0)
+	{
+		num1 = n1[--n1_len] - '0';
+	}
+	if (n2_len > 0)
+	{
+		num2 = n2[--n2_len] - '0';
+	}
+	int sum = num1 + num2 + carry;
+
+	carry = sum / 10;
+	r[--index] = (sum % 10) + '0';
+	if (n1_len == 0 && n2_len == 0 && carry > 0)
+		return (0);
+	}
+	if (n1_len > 0 || n2_len > 0 || carry > 0 || index != 0)
+		return (0);
+	return (r);
+}
