64c4a7c6f22b0ea47a8f830d5c3a6f2b14ae01e4
AliAsaad200 <you@example.com>
2023-09-21 23:39:41 +0300

    w

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/**
+* leet - integer is positive or negative
+* @s: first integer
+* Return: 0
+*/
+
+char *leet(char *s)
+{
+	char letter[] = "aAeEoOtTlL";
+	char new[] = "4433007711";
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letter[j] != NULL; j++)
+		{
+			if (s[i] == letter[j])
+			{
+				s[i] = new[j];
+				break;
+			}
+		}
+	}
+}
