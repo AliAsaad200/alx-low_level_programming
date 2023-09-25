d40890e8c1924af4de2adc589c1b12997c8ae0f0
AliAsaad200 <you@example.com>
2023-09-25 20:28:19 +0300

    w

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* set_string - integer is positive or negative
+* @s: first integer
+* @to: first integer
+* Return: 0
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+	return (s);
+}
