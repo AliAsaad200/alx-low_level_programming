ed2b353416ef676307c930e5c7ba749bee8022bb
AliAsaad200 <you@example.com>
2023-09-25 16:46:48 +0300

    w

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strchr - integer is positive or negative
+* @dest: first integer
+* @src: first integer
+* @n: first integer
+* Return: 0
+*/
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (s);
+}
