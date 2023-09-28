7d18fbaa0a8bd9daaf6793c790746a6ff228094f
AliAsaad200 <you@example.com>
2023-09-28 16:18:08 +0300

    w

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _puts_recursion - integer is positive or negative
+* @s: first integer
+* Return: 0
+*/
+void _puts_recursion(char *s)
+{
+	return (puts(s));
+}
