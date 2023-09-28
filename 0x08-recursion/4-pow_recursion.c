23fa93f984338f0c890b4f06e83e68c710d931b1
AliAsaad200 <you@example.com>
2023-09-28 18:11:55 +0300

    w

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,15 +10,13 @@
 */
 int _pow_recursion(int x, int y)
 {
-	int m;
-
 	if (y < 0)
 	{
 		return (-1);
 	}
 	else
 	{
-		 m = m*x;
+		int m = m*x;
 		printf("%d", m);
 		return(_pow_recursion(x , y));
 	}
