15b767937712bdb71935a1d60bc294e8ec73cdaf
AliAsaad200 <you@example.com>
2023-09-28 17:14:33 +0300

    w

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* factorial - integer is positive or negative
+* @n: first integer
+* Return: 0
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n + factorial(n - 1));
+	}
+}
