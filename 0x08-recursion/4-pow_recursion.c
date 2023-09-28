53bc8f0be753434313c244da7c7fc4c4f229fc38
AliAsaad200 <you@example.com>
2023-09-28 17:21:22 +0300

    w

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _pow_recursion - integer is positive or negative
+* @x: first integer
+* @y: first integer
+* Return: 0
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (pow(x, y));
+	}
+}
