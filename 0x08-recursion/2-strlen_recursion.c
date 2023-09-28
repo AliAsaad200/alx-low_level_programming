f6080ba9ee62bc8db9559df7097bb47e266967fc
AliAsaad200 <you@example.com>
2023-09-28 17:09:52 +0300

    w

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strlen_recursion - integer is positive or negative
+* @s: first integer
+* Return: 0
+*/
+int _strlen_recursion(char *s)
+{
+	int len = strlen(*s);
+
+	return(len);
+}
