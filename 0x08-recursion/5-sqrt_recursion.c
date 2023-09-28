54102e546a9570db69f512133b5071eb0d318f0e
AliAsaad200 <you@example.com>
2023-09-28 20:03:12 +0300

    w

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,34 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-/**
-* _sqrt_recursion - integer is positive or negative
-* @n: first integer
-* Return: 0
-*/
-int _sqrt_recursion(int n)
-{
-	int x = 0;
 
-	if (n < 0)
+/**
+ * sqrt - _pow
+ * @s: number
+ * @r: number
+ * Return: 0
+ */
+int sqrt(int s, int r)
+{
+	int Result = r * r;
+
+	if (Result == s)
+	{
+		return (r);
+	}
+	else if (Result < s)
+	{
+		return (Square(s, r + 1));
+	}
+	else
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	else if (x * x != n)
-	{
-		x = x + 1;
-		printf("%d", x);
-		return (0);
-	}
+}
+/**
+ * _sqrt_recursion - _pow
+ * @n: number
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	return (Square(n, 1));
 }
