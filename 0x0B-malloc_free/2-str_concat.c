d4efc47448ad4ffd8d6ad40b60079c3ccf7d4915
AliAsaad200 <you@example.com>
2023-10-04 00:05:30 +0300

    w

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,15 +10,21 @@
  char *str_concat(char *s1, char *s2)
 {
 	char *p;
+	int len1;
+	int len2;
+	int res;
 
 	if (s1 == NULL || s2 == NULL)
 	{
-		return (" ");
+		return ("");
 
 	}
 	else
 	{
-		p = malloc(sizeof(char) * strlen(s1 + 1) + strlen(s2 + 1));
+		len1 = strlen(s1 +1);
+		len2 = strlen(s2 +1);
+		res = len1 + len2;
+		p = malloc(sizeof(char) * res);
 		strcat (s1, s2);
 		printf ("%s", s1);
 		return (p);
