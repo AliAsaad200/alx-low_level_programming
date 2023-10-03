d7fa066d713ae0c6c412961fe3c544c0a7e360f4
AliAsaad200 <you@example.com>
2023-10-04 00:17:00 +0300

    w

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,25 +16,22 @@
 
 	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
 
 	}
 	if (s2 == NULL)
 	{
-		return ("");
+		s2 = "";
 	}
-	else
+	len1 = strlen(s1) + 1;
+	len2 = strlen(s2) + 1;
+	res = len1 + len2;
+	p = malloc(sizeof(char) * res);
+	if (p == NULL)
 	{
-		len1 = strlen(s1) + 1;
-		len2 = strlen(s2) + 1;
-		res = len1 + len2;
-		p = malloc(sizeof(char) * res);
-		if (p == NULL)
-		{
-			return (NULL);
-		}
-		strncat (p, s1, len1);
-		strncat (p, s2, len2);
-		return (p);
+		return (NULL);
 	}
+	strncat (p, s1, len1);
+	strncat (p, s2, len2);
+	return (p);
 }
