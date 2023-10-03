e4b7bbdd3919549053c8a7183480e39f746e03ca
AliAsaad200 <you@example.com>
2023-10-03 19:36:09 +0300

    w

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* str_concat - integer is positive or negative
+* @str: first integer
+* Return: 0
+*/
+ char *str_concat(char *s1, char *s2)
+{
+
+	if (s1 == NULL)
+	{
+		return (NULL);
+
+	}
+	s1 = malloc(strlen(s1 + 1) + strlen(s2 + 1));
+	strcat (s1, s2);
+	printf ("%s", s1);
+	free (s1);
+	return (0);
+}
