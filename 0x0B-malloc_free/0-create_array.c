57131c0555688df0833584693e73d0f449623655
AliAsaad200 <you@example.com>
2023-10-03 15:55:56 +0300

    w

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* create_array - integer is positive or negative
+* @size: first integer
+* @c: first integer
+* Return: 0
+*/
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+
+	}
+	else
+	{
+		char c = malloc(size);
+	}
+	free(size);
+}
