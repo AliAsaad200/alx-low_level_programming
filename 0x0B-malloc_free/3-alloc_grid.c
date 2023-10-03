efd1f6bddbee202dc7052658c80ed0b6766a6813
AliAsaad200 <you@example.com>
2023-10-04 00:30:02 +0300

    w

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* alloc_grid - integer is positive or negative
+* @width: first integer
+* @height: first integer
+* Return: 0
+*/
+nt **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+}
