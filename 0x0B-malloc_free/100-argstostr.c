156c7c0bf1c88bb4d81ea13e586cbdf1f08809d7
AliAsaad200 <you@example.com>
2023-10-04 01:51:57 +0300

    w

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * argstostr - pointer
+ * @ac: First Args
+ * @av: Sec Args
+ * Return: NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int current_position = 0;
+	int total_length = 0;
+	int i;
+	int x;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != NULL)
+		{
+			total_length += strlen(av[i]) + 1;
+		}
+	}
+
+	ptr = malloc((total_length + 1) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (x = 0; x < ac; x++)
+	{
+		if (av[x] != NULL)
+		{
+			strncpy(ptr + current_position, av[x], strlen(av[x]));
+			current_position += strlen(av[x]);
+			ptr[current_position] = '\n';
+			current_position++;
+		}
+	}
+
+	ptr[current_position] = '\0';
+
+	return (ptr);
+}
