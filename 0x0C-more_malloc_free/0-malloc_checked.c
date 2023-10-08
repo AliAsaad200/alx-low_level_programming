6ad54b5782dc823c78bfcf22970fb3591a365aed
AliAsaad200 <you@example.com>
2023-10-08 19:19:34 +0300

    w

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* malloc_checked - integer is positive or negative
+* @b: first integer
+* Return: 0
+*/
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p = 0;
+
+	if (p == NULL)
+	{
+		return ('98');
+
+	}
+	p = malloc(sizeof(int) * b);
+}
