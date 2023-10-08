c34fec5abd4329955270533313cdea9b160d8bda
AliAsaad200 <you@example.com>
2023-10-08 22:18:05 +0300

    w

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,6 +8,8 @@
 **/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *new_ptr;
+
 	if (new_size == old_size)
 		return (ptr);
 
@@ -20,8 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	void *new_ptr = malloc(new_size);
-
+	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 	{
 		return (NULL);
