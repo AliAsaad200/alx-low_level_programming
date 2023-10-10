9b5b0e9ee8e6215200510bd55f2ab6f7fcc946ea
AliAsaad200 <you@example.com>
2023-10-11 01:31:33 +0300

    w

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,30 +13,28 @@
  *
  * Return: Pointer to the newly created dog, or NULL if it fails.
  */
-dog_t *new_dog(char *name, float age, char *owner) {
-    dog_t *new_dog;
-    char *new_name, *new_owner;
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *new_name, *new_owner;
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	return (NULL);
 
-    new_name = malloc(sizeof(char) * (strlen(name) + 1));
-    new_owner = malloc(sizeof(char) * (strlen(owner) + 1));
-
-    if (new_name == NULL || new_owner == NULL) {
-        free(new_dog);
-        free(new_name);
-        free(new_owner);
-        return (NULL);
-    }
-
-    strcpy(new_name, name);
-    strcpy(new_owner, owner);
-
-    new_dog->name = new_name;
-    new_dog->age = age;
-    new_dog->owner = new_owner;
-
-    return (new_dog);
+	new_name = malloc(sizeof(char) * (strlen(name) + 1));
+	new_owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (new_name == NULL || new_owner == NULL)
+	{
+		free(new_dog);
+		free(new_name);
+		free(new_owner);
+		return (NULL);
+	}
+	strcpy(new_name, name);
+	strcpy(new_owner, owner);
+	new_dog->name = new_name;
+	new_dog->age = age;
+	new_dog->owner = new_owner;
+	return (new_dog);
 }
