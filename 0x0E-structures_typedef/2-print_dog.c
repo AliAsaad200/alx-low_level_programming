12a693b8ee1a8f149e5e77788be83ff4cca55bd6
AliAsaad200 <you@example.com>
2023-10-11 01:19:48 +0300

    w

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,9 +16,5 @@ void print_dog(struct dog *d)
 	{
 		printf("Age : %f\n", d->age);
 	}
-	else
-	{
-		printf("Age : (nil)\n");
-	}
 	printf("Owner : %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
