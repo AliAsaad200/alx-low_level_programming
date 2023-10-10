06326da157ea312813651858b1c5a401d7e2950b
AliAsaad200 <you@example.com>
2023-10-11 00:15:20 +0300

    w

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * big_multiply - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
+ *
+ * Return: the product big number string
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return 0;
+}
