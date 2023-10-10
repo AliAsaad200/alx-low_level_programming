099250d99684f490dfaab576adf1eed0291c3322
AliAsaad200 <you@example.com>
2023-10-11 01:05:35 +0300

    w

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,15 @@
 #include "dog.h"
 /**
-* malloc_checked - integer is positive or negative
-* @b: first integer
+* init_dog - integer is positive or negative
+* @d: first integer
+* @name: first integer
+* @age: first integer
+* @owner: first integer
 * Return: 0
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if( d != 0)
+	if (d != 0)
 	{
 		d->name = name;
 		d->age = age;
