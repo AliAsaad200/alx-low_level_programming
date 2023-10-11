0c26e58125e2cb033b53f1e0cc905a4dd6daccbb
AliAsaad200 <you@example.com>
2023-10-12 00:15:08 +0300

    w

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
-#include "main.h"
+#include "function_pointers.h"
+#include <stdio.h>
 /**
- * print_name - prints number of arguments passed
- * @name: tye int argument
- * @f: type char argument of strings
- * Return: 0
- */
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
-	{
+	if (name == NULL || f == NULL)
 		return;
-	}
+
 	f(name);
 }
