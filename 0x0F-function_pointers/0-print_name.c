1079959ebfc8c44d0dac2c6b4bdb6b6a542daf6e
AliAsaad200 <you@example.com>
2023-10-11 23:52:02 +0300

    w

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_name - prints number of arguments passed
+ * @name: tye int argument
+ * @f: type char argument of strings
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	printf("%s", name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+void print_name(char *name, void (*f)(char *));
+#endif /* MAIN_H */
