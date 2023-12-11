4d88eb196a2eae8eb9279cd3e5fde0abe83079b3
AliAsaad200 <aliahmedassad2003@gmail.com>
2023-12-12 01:06:46 +0200

    w

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -28,5 +28,15 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int _putchar(char c);
+void _puts(char *str);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+
 
 #endif /* MAIN_H */
